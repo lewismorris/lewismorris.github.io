417c672a83a57f5c50dc237bf195a6204f053a48
Lewis Morris <lewis@lewismorris.co.uk>
2017-12-20 17:15:38 +0000

    Inital commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const connect = require('gulp-connect');
+
+gulp.task('styles', function() {
+  return gulp.src(['./sass/**/*.scss'])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css'))
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: __dirname,
+    livereload: true
+  });
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['./sass/*.scss'], ['styles']);
+});
+
+gulp.task('build', ['styles']);
+gulp.task('default', ['build', 'connect', 'watch']);
